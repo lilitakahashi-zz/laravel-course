73970a144d862e529887eb51d7d913571aa911ef
lilitakahashi <lilitakahashi@gmail.com>
2021-12-15 14:30:46 -0300

    add content about loops

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Blog Posts')
 
 @section('content')
-    @foreach ($posts as $key => $post)
-        <div>{{ $key }}. {{ $post['title'] }}</div>
-    @endforeach    
+    @forelse ($posts as $key => $post)
+        @include('posts.partials.post', [''])
+    @empty
+        No posts found!
+    @endforelse    
 @endsection
