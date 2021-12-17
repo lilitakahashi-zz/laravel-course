3c497ec28d2253f6ecd874f46b73988054bbf4c6
lilitakahashi <lilitakahashi@gmail.com>
2021-12-17 17:46:29 -0300

    37 Redirect Responses

--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,19 @@ Route::get('/fun/responses', function() use($posts) {
     ->header('Content-Type', 'application/json')
     ->cookie('MY_COOKIE', 'Lili Takahashi', 3600);
 });
+
+Route::get('/fun/redirect', function() {
+    return redirect('/contact');
+});
+
+Route::get('/fun/back', function() {
+    return back();
+});
+
+Route::get('/fun/named-route', function() {
+    return redirect()->route('posts.show', ['id' => 1]);
+});
+
+Route::get('/fun/away', function() {
+    return redirect()->away('https://google.com');
+});
