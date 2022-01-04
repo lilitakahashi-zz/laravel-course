9696c7f34404c30b944a497376f1b7d0d9d9f7dd
lilitakahashi <lilitakahashi@gmail.com>
2022-01-04 20:00:03 -0300

    65 Session Flash Messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel App - @yield('title')</title>
 </head>
+
 <body>
     <div>
+        @if (session('status'))
+            <div style="background: red; color: white">
+                {{ session('status') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
